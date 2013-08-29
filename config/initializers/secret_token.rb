3a735ee4c7799f6c88d8f3b72376f69914c4b738
makino1121 <keita.makino@h-theday.jp>
2013-08-30 01:38:20 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commune::Application.config.secret_token = '4412837d922797c225e985546bf4c34b1f2ecd2ae28ef2317df2dff1ac6256a51301bc080742891941c0bcd3e35a3402f7eef61e212fe76512330dfc2777a4ab'
